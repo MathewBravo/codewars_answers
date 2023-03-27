ed74417401c45c1e1f3b5ad4c9e6e5c38ced23f4
mathew <mathewbravo94@gmail.com>
2023-03-27 09:35:22 -0400

    Valid Bracers done

---
+++ valid_braces/main.py
@@ -0,0 +1,49 @@
+def valid_braces(string) -> bool:
+    bracket_track = []
+    for ch in string:
+        if ch == "{":
+            bracket_track.append("c_open")
+        elif ch == "(":
+            bracket_track.append("n_open")
+        elif ch == "[":
+            bracket_track.append("s_open")
+        elif ch == "}":
+            contains = False
+            if len(bracket_track) == 0:
+                return False
+            if bracket_track[len(bracket_track) - 1] == "c_open":
+                bracket_track.remove("c_open")
+                contains = True
+            if not contains:
+                return False
+        elif ch == ")":
+            contains = False
+            if len(bracket_track) == 0:
+                return False
+            if bracket_track[len(bracket_track) - 1] == "n_open":
+                bracket_track.remove("n_open")
+                contains = True
+            if not contains:
+                return False
+        elif ch == "]":
+            contains = False
+            if len(bracket_track) == 0:
+                return False
+            if bracket_track[len(bracket_track) - 1] == "s_open":
+                bracket_track.remove("s_open")
+                contains = True
+            if not contains:
+                return False
+    if len(bracket_track) > 0:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    if not valid_braces("[(])"):
+        print("1 Pass")
+    if not valid_braces("())({}}{()][]["):
+        print("2 Pass")
+    if valid_braces("(({{[[]]}}))"):
+        print("3 Pass")
