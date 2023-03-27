44fb954b06ea5eded24bc89de44b00d3ea036ab1
mathew <mathewbravo94@gmail.com>
2023-03-27 17:09:06 -0400

    Max sub finished

--- max_sub_sum/main.py
+++ max_sub_sum/main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def max_sequence(arr) -> int:
+    best_sum = 0
+    current_sum = 0
+    for x in arr:
+        current_sum = max(0, current_sum + x)
+        best_sum = max(best_sum, current_sum)
+    return best_sum
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6:
+        print("Pass")
+    else:
+        print("returned value:" + str(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])))
+    if max_sequence([7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43]) == 155:
+        print("Pass")
+    else:
+        print(
+            "returned value:" + str(max_sequence([7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43])))
